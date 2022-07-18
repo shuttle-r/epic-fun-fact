60641d959d26906b0a64fb07763687ce898bdfab
shuttle-r <109020578+shuttle-r@users.noreply.github.com>
2022-07-18 12:25:57 +0800

    Update funfact.lua

--- funfact.lua
+++ funfact.lua
@@ -111,7 +111,7 @@ local function loadingarea_Wraping() -- TextButton.LocalScript
 	local script = Instance.new('LocalScript', TextButton)
 
 	local state = true
-	task.wait(5)
+	task.wait(15)
 	
 		
 		state = not state
